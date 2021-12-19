e986c08530622fc63dc39203926de7b0cff29dc4
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2021-12-19 22:49:28 +0100

    use JndiLookupConstructorWithISException

--- src/test/java/com/github/pfichtner/log4shell/scanner/Log4jSamplesIT.java
+++ src/test/java/com/github/pfichtner/log4shell/scanner/Log4jSamplesIT.java
@@ -1,6 +1,5 @@
 package com.github.pfichtner.log4shell.scanner;
 
-import static com.github.pfichtner.log4shell.scanner.detectors.IsJndiEnabledPropertyAccess.LOG4J2_ENABLE_JNDI;
 import static com.github.pfichtner.log4shell.scanner.io.Files.isArchive;
 import static java.nio.file.Files.walk;
 import static java.util.stream.Collectors.toList;
@@ -17,6 +16,7 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DynamicTest;
@@ -29,7 +29,7 @@ import com.github.pfichtner.log4shell.scanner.CVEDetector.Detection;
 import com.github.pfichtner.log4shell.scanner.Detectors.Multiplexer;
 import com.github.pfichtner.log4shell.scanner.detectors.AbstractDetector;
 import com.github.pfichtner.log4shell.scanner.detectors.InitialContextLookupsCalls;
-import com.github.pfichtner.log4shell.scanner.detectors.IsJndiEnabledPropertyAccess;
+import com.github.pfichtner.log4shell.scanner.detectors.JndiLookupConstructorWithISException;
 import com.github.pfichtner.log4shell.scanner.detectors.JndiManagerLookupCallsFromJndiLookup;
 import com.github.pfichtner.log4shell.scanner.detectors.Log4jPluginAnnotation;
 import com.github.pfichtner.log4shell.scanner.detectors.Log4jPluginAnnotationObfuscateAwareClassNodeCollector;
@@ -125,28 +125,55 @@ public class Log4jSamplesIT {
 
 		// TODO verify if the class found by vulns are plugins
 		Log4jPluginAnnotation isPlugin = new Log4jPluginAnnotation();
-		IsJndiEnabledPropertyAccess isJndiEnabledPropertyAccess = new IsJndiEnabledPropertyAccess();
+		JndiLookupConstructorWithISException isAtLeastLog4J217 = new JndiLookupConstructorWithISException();
 
 		List<AbstractDetector> all = new ArrayList<>(vulns);
-		all.add(isJndiEnabledPropertyAccess);
+		all.add(isAtLeastLog4J217);
 
 		return new Multiplexer(all) {
 
 			@Override
 			public void visitEnd() {
 				super.visitEnd();
-				List<Detector> detectors = getDetections().stream().map(Detection::getDetector).collect(toList());
+				List<Detector> detectors = getDetections().stream().filter(isFalsePositivesLog4J().negate())
+						.map(Detection::getDetector).collect(toList());
 
 				// if we have Detections on classes (Paths) one of vulns, this is vulnerable IF
 				// NOT we also have isJndiEnabledPropertyAccess
 
 				boolean isVuln = vulns.stream().anyMatch(v -> detectors.contains(v));
-				boolean hasPropertyAccess = detectors.contains(isJndiEnabledPropertyAccess);
+				boolean hasPropertyAccess = detectors.contains(isAtLeastLog4J217);
 
 				if (isVuln && !hasPropertyAccess) {
-					System.err.println(getResource() + ": Log4J version with context lookup found (without "
-							+ LOG4J2_ENABLE_JNDI + " check)");
+					System.err.println(getResource()
+							+ ": Log4J version with context lookup found (without code introduced in log4j-2.17)");
 				}
+			}
+
+			/**
+			 * Filter out false positives on naming context lookups. In fact this is only
+			 * needed when running in mode where classnames and methodnames are ignored
+			 * (obfuscated).
+			 */
+			private Predicate<Detection> isFalsePositivesLog4J() {
+				return new Predicate<Detection>() {
+
+					List<String> classesWithNamingContexLookupCalls = Arrays.asList( //
+							"/net/JMSSink.class", //
+							"/net/JMSAppender.class", //
+							"/core/selector/JNDIContextSelector.class", //
+							"/core/net/JMSTopicReceiver.class", //
+							"/core/net/JMSQueueReceiver.class", //
+							"/core/net/AbstractJMSReceiver.class", //
+							"/core/net/AbstractJMSManager.class");
+
+					@Override
+					public boolean test(Detection detection) {
+						return detection.getDetector().getClass() == NamingContextLookupCallsFromJndiLookup.class
+								&& classesWithNamingContexLookupCalls.stream()
+										.anyMatch(detection.getFilename().toString()::endsWith);
+					}
+				};
 			}
 		};
 
