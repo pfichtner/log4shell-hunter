72871c84ccd9071534b09d6fe1f8e85d9458b7be
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-10-29 16:08:26 +0100

    exacter match on method called

--- src/main/java/com/github/pfichtner/log4shell/scanner/detectors/IsJndiEnabledPropertyAccess.java
+++ src/main/java/com/github/pfichtner/log4shell/scanner/detectors/IsJndiEnabledPropertyAccess.java
@@ -8,14 +8,18 @@ import static com.github.pfichtner.log4shell.scanner.util.AsmUtil.returnTypeIs;
 import static com.github.pfichtner.log4shell.scanner.util.Streams.filter;
 import static org.objectweb.asm.Opcodes.ICONST_0;
 import static org.objectweb.asm.Type.BOOLEAN_TYPE;
+import static org.objectweb.asm.Type.getObjectType;
 
 import java.nio.file.Path;
 import java.util.function.Predicate;
 
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
+
+import com.github.pfichtner.log4shell.scanner.util.AsmTypeComparator;
 
 /**
  * Searches for methods that loads {@value #LOG4J2_ENABLE_JNDI} from the
@@ -45,8 +49,22 @@ public class IsJndiEnabledPropertyAccess extends AbstractDetector {
 	private static final Predicate<AbstractInsnNode> callGetBooleanPropertyWithFalse = hasOpCode(ICONST_0)
 			.and(n -> isCallToMethodThatReturnsBoolean(n.getNext()));
 
+	private static final Type PROPERTIESUTIL_TYPE = getObjectType("org/apache/logging/log4j/util/PropertiesUtil");
+
 	private static boolean isCallToMethodThatReturnsBoolean(AbstractInsnNode node) {
-		return node instanceof MethodInsnNode && returnTypeIs((MethodInsnNode) node, BOOLEAN_TYPE);
+		if (!(node instanceof MethodInsnNode)) {
+			return false;
+		}
+		MethodInsnNode methodInsnNode = (MethodInsnNode) node;
+		if (!returnTypeIs(methodInsnNode, BOOLEAN_TYPE)) {
+			return false;
+		}
+
+		AsmTypeComparator typeComparator = AsmTypeComparator.typeComparator();
+		return typeComparator == AsmTypeComparator.obfuscatorComparator
+				|| (typeComparator.isClass(getObjectType(methodInsnNode.owner), PROPERTIESUTIL_TYPE)
+						&& "getBooleanProperty".equals(methodInsnNode.name)
+						&& "(Ljava/lang/String;Z)Z".equals(methodInsnNode.desc));
 	}
 
 	@Override
